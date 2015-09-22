5e5276ae0de655bbf514ae3614356ef868bbd331
tommyfok <154274174@qq.com>
2015-09-22 17:37:41 +0800

    minor change

--- ngTree.js
+++ ngTree.js
@@ -1,7 +1,7 @@
 var ngTreeTpl = ''
 +'<li class="ngTree" ng-repeat="child in parent" ng-class="{hasChild: child.children.length, opened: child.ngTreeShow}">'
 +'  <a ng-bind="child.text" ng-click="handleItem(child)"></a>'
-+'  <ul ng-show="child.ngTreeShow" ng-if="child.children" ng-include="\'ngTreeTpl\'" ng-init="parent=child.children"></ul>'
++'  <ul ng-show="child.ngTreeShow" ng-if="child.children.length" ng-include="\'ngTreeTpl\'" ng-init="parent=child.children"></ul>'
 +'</li>';
 
 var ngTreeStyle = '<style>.ngTree{-webkit-user-select:none}.ngTree>a{padding-left:13px}.ngTree.hasChild{position:relative}.ngTree.hasChild>a{font-weight:bold}.ngTree.hasChild:before{content:"";width:0;height:0;left:0;top:10px;border:5px solid transparent;border-left-color:#aaa;position:absolute}.ngTree.hasChild.opened:before{border-left-color:transparent;border-top-color:#aaa;left:-3px;top:13px}.ngTree,.ngTree li{cursor:pointer;list-style:none;line-height:30px}.ngTree ul{padding:0;margin:0 2px;padding-left:7px;border-left:1px dotted #ddd}</style>';
